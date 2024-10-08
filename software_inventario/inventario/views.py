e5cabd61bc8b720966fcfbea722f556e24f8b92c
Daniel <dfespinal7sena@gmail.com>
2024-10-08 15:20:06 -0500

    cambios

--- software_inventario/inventario/views.py
+++ software_inventario/inventario/views.py
@@ -627,5 +627,6 @@ def listar_abc(request):
         i.sumat=sumatotal
         i.porcent=(sumatotal/totalinvent*100) if totalinvent else 0   
     abc = sorted(abc, key=lambda x: sum([int(e.valorUnidad) * int(e.cantEntInicial) for e in Entradas.objects.filter(idProducto=x.idProducto)]), reverse=True) 
+    
     contex={"data":abc,}
     return render(request,'inventario/stock/indicadoresabc.html',contex)
